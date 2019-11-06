1208ba3c1af3da584fa1293fbbac92f72bc56cc0
YuichiYagisawa <yuichi9915@gmail.com>
2019-11-06 12:29:51 +0900

    add color palet

--- src/resources/views/author/create.blade.php
+++ src/resources/views/author/create.blade.php
@@ -46,7 +46,7 @@
                 <input type="hidden" value="{{$author->id}}" name="author_id">
 
                 <div class="row radio-tile-group mb-5">
-                    <div class="col-lg-3 input-container">
+                    {{-- <div class="col-lg-3 input-container">
                         <input id="walk" class="radio-button red" type="radio" name="color" value="#FF4545"/>
                         <div class="radio-tile"
                              style="background: linear-gradient(315deg, rgba(255,150,150,1) 0%, rgba(255,69,69,1) 100%);">
@@ -140,7 +140,199 @@
                              style="background: linear-gradient(315deg, rgba(250,250,250,1) 0%, rgba(240,240,240,1) 100%);">
                         </div>
                         <label for="fly" class="radio-tile-label">White</label>
-                    </div>
+                    </div> --}}
+
+                        <div class="col-lg-3 input-container">
+                            <input id="walk" class="radio-button red" type="radio" name="color" value="#723132"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,150,150,1) 0%, rgba(255,69,69,1) 100%);">
+                            </div>
+                            <label for="walk" class="radio-tile-label">焦げ茶</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="bike" class="radio-button pink" type="radio" name="color" value="#BF4638"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,150,239,1) 0%, rgba(255,69,226,1) 100%);">
+                            </div>
+                            <label for="bike" class="radio-tile-label">茶</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="drive" class="radio-button purple" type="radio" name="color" value="#F03133"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(198,150,255,1) 0%, rgba(155,69,255,1) 100%);">
+                            </div>
+                            <label for="drive" class="radio-tile-label">赤</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="fly" class="radio-button navy" type="radio" name="color" value="#F1742E"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(150,158,255,1) 0%, rgba(69,90,255,1) 100%);">
+                            </div>
+                            <label for="fly" class="radio-tile-label">朱</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="walk" class="radio-button blue" type="radio" name="color" value="#EEA82D"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(150,211,255,1) 0%, rgba(69,183,255,1) 100%);">
+                            </div>
+                            <label for="walk" class="radio-tile-label">橙</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="bike" class="radio-button skyBlue" type="radio" name="color" value="#E4C71A"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(150,255,243,1) 0%, rgba(69,255,234,1) 100%);">
+                            </div>
+                            <label for="bike" class="radio-tile-label">蜜柑</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="drive" class="radio-button green" type="radio" name="color" value="#EFCE8A"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(150,255,166,1) 0%, rgba(69,255,98,1) 100%);">
+                            </div>
+                            <label for="drive" class="radio-tile-label">肌色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="fly" class="radio-button lime" type="radio" name="color" value="#EEF419"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(219,255,150,1) 0%, rgba(191,255,69,1) 100%);">
+                            </div>
+                            <label for="fly" class="radio-tile-label">檸檬</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="walk" class="radio-button yellow" type="radio" name="color" value="#DFDA20"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,243,150,1) 0%, rgba(255,226,69,1) 100%);">
+                            </div>
+                            <label for="walk" class="radio-tile-label">黄色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="bike" class="radio-button orange" type="radio" name="color" value="#B6A124"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,190,150,1) 0%, rgba(255,133,69,1) 100%);">
+                            </div>
+                            <label for="bike" class="radio-tile-label">黄土</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="drive" class="radio-button black" type="radio" name="color" value="#8ABC3A"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(85,85,85,1) 0%, rgba(17,17,17,1) 100%);">
+                            </div>
+                            <label for="drive" class="radio-tile-label">黄緑</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="fly" class="radio-button white" type="radio" name="color" value="#3EAB61"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(250,250,250,1) 0%, rgba(240,240,240,1) 100%);">
+                            </div>
+                            <label for="fly" class="radio-tile-label">緑色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="walk" class="radio-button yellow" type="radio" name="color" value="#3E7D3A"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,243,150,1) 0%, rgba(255,226,69,1) 100%);">
+                            </div>
+                            <label for="walk" class="radio-tile-label">深緑</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="bike" class="radio-button orange" type="radio" name="color" value="#3D9BE9"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,190,150,1) 0%, rgba(255,133,69,1) 100%);">
+                            </div>
+                            <label for="bike" class="radio-tile-label">水色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="drive" class="radio-button black" type="radio" name="color" value="#3871C3"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(85,85,85,1) 0%, rgba(17,17,17,1) 100%);">
+                            </div>
+                            <label for="drive" class="radio-tile-label">青色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="fly" class="radio-button white" type="radio" name="color" value="#275798"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(250,250,250,1) 0%, rgba(240,240,240,1) 100%);">
+                            </div>
+                            <label for="fly" class="radio-tile-label">藍色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="walk" class="radio-button yellow" type="radio" name="color" value="#38319A"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,243,150,1) 0%, rgba(255,226,69,1) 100%);">
+                            </div>
+                            <label for="walk" class="radio-tile-label">群青</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="bike" class="radio-button orange" type="radio" name="color" value="#8B52A1"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,190,150,1) 0%, rgba(255,133,69,1) 100%);">
+                            </div>
+                            <label for="bike" class="radio-tile-label">紫色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="drive" class="radio-button black" type="radio" name="color" value="#CE3D9C"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(85,85,85,1) 0%, rgba(17,17,17,1) 100%);">
+                            </div>
+                            <label for="drive" class="radio-tile-label">赤紫</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="fly" class="radio-button white" type="radio" name="color" value="#F2A1C2"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(250,250,250,1) 0%, rgba(240,240,240,1) 100%);">
+                            </div>
+                            <label for="fly" class="radio-tile-label">桃色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="walk" class="radio-button yellow" type="radio" name="color" value="#AF8C3C"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,243,150,1) 0%, rgba(255,226,69,1) 100%);">
+                            </div>
+                            <label for="walk" class="radio-tile-label">朽葉</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="bike" class="radio-button orange" type="radio" name="color" value="#BDB7BD"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(255,190,150,1) 0%, rgba(255,133,69,1) 100%);">
+                            </div>
+                            <label for="bike" class="radio-tile-label">灰色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="drive" class="radio-button black" type="radio" name="color" value="##000E00"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(85,85,85,1) 0%, rgba(17,17,17,1) 100%);">
+                            </div>
+                            <label for="drive" class="radio-tile-label">黒色</label>
+                        </div>
+    
+                        <div class="col-lg-3 input-container">
+                            <input id="fly" class="radio-button white" type="radio" name="color" value="#f0f0f0"/>
+                            <div class="radio-tile"
+                                 style="background: linear-gradient(315deg, rgba(250,250,250,1) 0%, rgba(240,240,240,1) 100%);">
+                            </div>
+                            <label for="fly" class="radio-tile-label">白色</label>
+                        </div>
                 </div>
                 <button type="submit" role="button" data-slide="next" class="arrow_btn w-100 start">
                     レビューする
